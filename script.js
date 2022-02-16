865bd29bda5840967a2f7c229453c62d843ea619
Sergey13111 <boiko.sergey93@gmail.com>
2022-02-16 02:09:13 +0200

    added task-3,4

--- script.js
+++ script.js
@@ -1,50 +1,67 @@
 "use strict";
 
 // 1
-// const dateBirth = new Date(1984, 7, 3);
-// let day = dateBirth.getDate();
-// if (day < 10) {
-//     day = "0" + day; 
-// };
-// let month = dateBirth.getMonth() + 1;
-// if (month < 10) {
-//     month = "0" + month; 
-// };
-// let year = dateBirth.getFullYear();
+const dateBirth = new Date(1984, 7, 3);
+let day = dateBirth.getDate();
+if (day < 10) {
+    day = "0" + day; 
+};
+let month = dateBirth.getMonth() + 1;
+if (month < 10) {
+    month = "0" + month; 
+};
+let year = dateBirth.getFullYear();
 
-// console.log(`${day}.${month}.${year}`);
+console.log(`${day}.${month}.${year}`);
 
 // 2
 
 const getDiffDays = (dateStart, dateEnd) => {
     let start = new Date(dateStart);
     let end = new Date(dateEnd);
-    console.log(start)
+
     if ((start || end) == 'Invalid Date') {
-        
-        console.error('invalid date!!!!!') 
+        console.error('Error: invalid date!!!!!');
         return;
     };
+
     if (start > end) {
-        console.error('your start date is later than end');
+        console.error('Error: your start date is later than end');
         return;
     };
+
     let diffDays = ((+end) - (+start)) / (1000 * 3600 * 24);
     return diffDays;
 };
 
-console.log(getDiffDays('2020-01-01', '2020-01-17')); // 16
-console.log(getDiffDays('2020-01-01', '2020-03-15')); // 74
-сonsole.log(getDiffDays('2222222222', '2020-03-15')); // Error: invalid date
+console.log(getDiffDays('2020-01-01', '2020-01-17'));
+console.log(getDiffDays('2020-01-01', '2020-03-15')); 
+console.log(getDiffDays('2222222222', '2020-03-15'));
 console.log(getDiffDays('2021-01-02', '2020-03-15'));
 
+// 3
+const isWeekend = (date) => {
+    let weekend = new Date(date);
+    return (weekend.getDay() === 0 || weekend.getDay() === 6);
+};
 
+console.log(isWeekend('2022-02-12'));
+console.log(isWeekend('2022-02-13')); 
+console.log(isWeekend('2022-02-09')); 
 
+// 4
+const person = {
+    fullName: 'Sherlock Holmes',
+    address: {
+        street: "Baker Street",
+        city: "London",
+        house: "221b"
+    }
+}
 
-    // let start = new Date('2222222222');
-    // let end = new Date('2020-03-15');
-    // if ((start || end) == 'Invalid Date') {
-                
-    //             console.error('invalid date!!!!!!!');
-            
-    //         };
+const jsonPerson = JSON.stringify(person);
+const newPerson = JSON.parse(jsonPerson);
+const {fullName, address: {street, city, house}} = person;
+console.log(jsonPerson);
+console.log(newPerson);
+console.log(fullName, street, city, house);
